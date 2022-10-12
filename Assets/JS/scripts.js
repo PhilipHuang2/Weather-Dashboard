ace1689dd388f15af166df1078af64f0e57eaf50
Philip Huang <philiphuang2@gmail.com>
2022-10-11 21:36:54 -0700

    when I press a valid address, I should see the Latitude and Longitude of the City

--- Assets/JS/scripts.js
+++ Assets/JS/scripts.js
@@ -1,12 +1,16 @@
 API_KEY = "bd16f5ef224423a097f3ef14062bba6a";
-var geoLocate = 'http://api.openweathermap.org/geo/1.0/direct?q=${city name},${state code},${country code}&limit=${limit}&appid=${API key}'
-var addressForm = $("#addressForm")
+var addressForm = $("#addressForm");
+var results = $("#resultColumn");
 
 addressForm.on('click', function(target){
     target.preventDefault();
     geoEndpoint($('#inputAddress').val());
 });
 
+
+// Valid City Name
+// Invalid City Name
+// empty String
 function geoEndpoint(name, state="", country="", limit="", apiKey = API_KEY)
 {
     var geoLocate = 'http://api.openweathermap.org/geo/1.0/direct?q=';
@@ -18,7 +22,20 @@ function geoEndpoint(name, state="", country="", limit="", apiKey = API_KEY)
     fetch(geoLocate)
     .then((response) => response.json())
     .then((data)=> {
-        console.log(data);
+        if(data.length)
+        {
+            console.log(data[0].lat);
+            console.log(data[0].lon);
+            console.log("Latitude: " + data[0].lat + "/nLongitude: " + data[0].lon);
+            results.text("Latitude: " + data[0].lat + " Longitude: " + data[0].lon);
+            
+
+        }
+        else
+        {
+            console.log("This city doesn't exist.  Try again");
+        }
+        
     })
     .catch(console.error);
 }
